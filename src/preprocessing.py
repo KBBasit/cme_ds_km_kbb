a76d71deb6c22f8ae8b5417b37acd02ef3875db7
Kiarash Basit <kiarash-2005@outlook.com>
2026-08-16 20:59:05 +0100

    Change translator function to use deep-translator library

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,17 +1,24 @@
-from googletrans import Translator
+from langdetect import detect
+from deep_translator import GoogleTranslator
 
 
 
-async def translate_text(text):
+def translate_text_to_english(text_list):
     """
-    Translates the given text to English using the googletrans library.
+    Translates the given text to English using Google Translator. Checks 
+    the language of each text string and translates only if it is not already
+    in English.
     Args:
-        text (str): The text to be translated.
-
+        text_list (list): List of text strings to be translated into English.
     Returns:
-        str: The translated text in English.
+        list: A list of translated text strings in English.
     """
-    translator = Translator()
-    translation = await translator.translate(text, dest='en')
+    translated_text_list = []
+    for i in text_list:
+        if detect(i) == "en":
+            translated_text_list.append(i)
+        else:
+            translated_text = GoogleTranslator(source='auto', target='en').translate(i)
+            translated_text_list.append(translated_text)
 
-    return translation.text
+    return translated_text_list
