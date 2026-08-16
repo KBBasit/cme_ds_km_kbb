5a5935de890049555cb775a51dc8ba428a3c803d
Kiarash Basit <kiarash-2005@outlook.com>
2026-08-16 10:20:26 +0100

    Implement function for sentiment analysis

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,1 +1,21 @@
 from transformers import pipeline
+
+
+
+def sentiment_analysis(text_list):
+    """
+    Perform sentiment analysis on the given text using a pre-trained model.
+    Args:
+        text_list (list): A list of input texts to analyze.
+    Returns:
+        list: A list of dictionaries containing the sentiment labels and scores.
+    """
+    # Load the sentiment analysis pipeline
+    sentiment_analyser = pipeline("sentiment-analysis", model="ProsusAI/finbert")
+
+    results = []
+    for text in text_list:
+        result = sentiment_analyser(text[:500])
+        results.append(result[0])
+
+    return results
